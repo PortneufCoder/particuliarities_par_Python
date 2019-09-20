aa56aa336e1694c506d36a9950a4f28428537f91
Victor Ajani <portneufcoder@gmail.com>
2019-09-19 21:24:07 -0400

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+print("HELLO World")
+a = 40
+
+b = 25
+
+c = 30
+
+print(a + b)
+
+name = "Victor"
+
+school = "Udacity"
+
+print("Happy days {0}. You want to learn at {1}".format(name, school))
+
---
+++ If.py
@@ -0,0 +1,13 @@
+age = 33
+
+if age == 33:
+    print("That is a nice age")
+else:
+    print("You're still a youngin'!")
+
+student_names = []
+student_names = ["Jake", "Thomas", "Sebastien"]
+student_names.extend("Homer")
+print(student_names)
+print(len(student_names))
+print(student_names[2:])
