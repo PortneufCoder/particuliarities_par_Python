df39b7825e678649327a1a1ba0af7800798f94db
Victor Ajani <portneufcoder@gmail.com>
2019-09-23 20:40:13 -0400

    testing args n kwargs

--- functions.py
+++ functions.py
@@ -13,10 +13,36 @@ def print_students_titlecase():
     print(students_titlecase)
 
 
-def add_student(name):
-    students.append(name)
+# assume student_id param has value of 123 if
+# nothing else is passed for it function call.
+
+
+def add_student(name, student_id=123):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
 
 
 student_list = get_students_titlecase()
 
-add_student("Victor")
+add_student(name="Victor", student_id=10)
+
+
+# Setting an unknown number of arguments *args
+
+
+def var_arguments(name, *args):
+    print(name)
+    print(args)
+
+
+var_arguments("Esmerelda", "Loves Python", 123, None, False)
+
+# Using kwargs to add params with a key
+
+
+def kwargs_test(country, **kwargs):
+    print(country)
+    print(kwargs["temp"], kwargs["aliens_present"])  # prints the params with those keys
+
+
+kwargs_test("Pluto", description="Solar System", temp="cold", aliens_present="maybe")
