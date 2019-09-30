b54c3cc3fab9c08c2c3c2f43a7ad07b44bd87e9d
Victor Ajani <portneufcoder@gmail.com>
2019-09-29 21:52:09 -0400

    cleanup

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ students = []
 def get_students_titlecase():
     students_titlecase = []
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
 
@@ -14,17 +14,30 @@ def print_students_titlecase():
 
 
 # assume student_id param has value of 123 if
-# nothing else is passed for it function call.
+# nothing else is passed for it in function call.
 
 
 def add_student(name, student_id=123):
     student = {"name": name, "student_id": student_id}
-    students.append(student)
+    if student:
+        students.append(student)
+    else:
+        new_entry = "No"
+    print(students)
+    return
 
 
 student_list = get_students_titlecase()
 
-add_student(name="Victor", student_id=10)
+
+student_name = input("Enter student name: ")
+student_id = input("Enter student ID: ")
+
+add_student(student_name, student_id)
+print_students_titlecase()
+
+
+# add_student(name="Victor", student_id=10)
 
 
 # Setting an unknown number of arguments *args
@@ -37,12 +50,13 @@ def var_arguments(name, *args):
 
 var_arguments("Esmerelda", "Loves Python", 123, None, False)
 
+
 # Using kwargs to add params with a key
 
 
 def kwargs_test(country, **kwargs):
     print(country)
-    print(kwargs["temp"], kwargs["aliens_present"])  # prints the params with those keys
+    print(kwargs["temp"], kwargs["aliens_present"])  # prints the values with those keys
 
 
 kwargs_test("Pluto", description="Solar System", temp="cold", aliens_present="maybe")
