95b3d9dfc812e6c3016118319b863f2a4f8f8248
Victor Ajani <portneufcoder@gmail.com>
2019-10-02 20:21:26 -0400

    inheritance and polymorphism in python

--- python_OOP/classes.py
+++ python_OOP/classes.py
@@ -2,8 +2,36 @@ students = []
 
 
 class Student:
-    def add_student(self, name, student_id=123):  # self == this in JS
-        student = {"name": name, "student_id": student_id}
-        students.append(student)
+    school_name = "Springfield Elementary"  # can be called directly since its a class attribute ! an instance attribute
+
+    def __init__(self, name, student_id=123):  # self == this in JS
+        self.name = name
+        self.student_id = student_id
+        students.append(self)  # append all attributes to list
+
+    def __str__(self):  # this method overrides the first method
+        return "Student's name is " + self.name
+
+    def get_name_capitalized(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
 
 
+homer = Student("Homer")  # class initialization
+
+
+class HighSchoolStudent(Student):  # this subclass(derived) can access all methods and vars in parent
+    school_name = "Springfield High School"
+
+    def get_school_name(self):
+        return "This student attends High School"
+
+    def get_name_capitalized(self):
+        original_value = super().get_name_capitalized()  # super gets the method from the parent
+        return original_value + "-HS"
+
+
+bart = HighSchoolStudent("bart")
+print(bart.get_name_capitalized())
