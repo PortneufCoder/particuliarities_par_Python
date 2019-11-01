73da30d43926552f5dd61aff1f65aee6e7dd575f
Victor Ajani <portneufcoder@gmail.com>
2019-10-31 22:10:21 -0400

    experimenting list methods

--- exercices.py
+++ exercices.py
@@ -1,4 +1,3 @@
-
 numbers = [12, 34, 36, 12452, 75]
 
 ordered = sorted(numbers, reverse=True)
@@ -7,3 +6,14 @@ print(ordered)
 
 new_str = "\n".join(["fore", "aft", "starboard", "port"])
 print(new_str)
+
+test_list = ["bread", "rice", "flax", "lentils", "beans"]
+
+length = len(test_list)
+maximum = max(test_list)
+minimum = min(test_list)
+print(length)
+print(maximum)
+print(minimum)
+
+print(test_list[0:3])  # ['bread', 'rice', 'flax']
