50ed91a1e130cead4a085899fa948bea811bc031
Victor Ajani <portneufcoder@gmail.com>
2019-11-05 22:19:54 -0500

    practicing dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -15,3 +15,28 @@ for student in all_students:
 
 print(student.keys())
 print(student.values())
+
+cast = {
+    "Jerry Seinfeld": "Jerry Seinfeld",
+    "Julia Louis-Dreyfus": "Elaine Benes",
+    "Jason Alexander": "George Costanza",
+    "Michael Richards": "Cosmo Kramer"
+}
+
+print(dir(cast))
+#  If you wish to iterate through both keys and values, you can use the built-in method items() like this:
+for key, value in cast.items():
+    print("Actor: {}    Role: {}".format(key, value))
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Iterate through the dictionary
+for stuff, value in basket_items.items():
+    if stuff in fruits:
+        result += value
+# if the key is in the list of fruits, add the value (number of fruits) to result
+
+
+print(result)
